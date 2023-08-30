2d6b3c5c7d14f711a7a93e2e77d75eeac693bc84
Руслан <Stariy123@mail.ru>
2023-08-30 14:32:22 +0300

    Final Version

--- src/CompareLines.java
+++ src/CompareLines.java
@@ -1,29 +1,29 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FilterOutputStream;
-import java.sql.Array;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CompareLines {
     public static void main(String[] args) {
-
-
-
         try {
-            new BufferedReader(new FileReader("input.txt")).lines()
+            Double maxLength = new BufferedReader(new FileReader("input.txt")).lines()
                     .map(s -> {
-                        String onlyNumbers = s.replaceAll("[^0-9]", "");
-                        double x1 = Double.parseDouble(String.valueOf(onlyNumbers.charAt(0)));
-                        double y1 = Double.parseDouble(String.valueOf(onlyNumbers.charAt(1)));
-                        double x2 = Double.parseDouble(String.valueOf(onlyNumbers.charAt(2)));
-                        double y2 = Double.parseDouble(String.valueOf(onlyNumbers.charAt(3)));
-
+                        String onlyNumbers = s.replaceAll("[^0-9]", " ");
+                        String[] onlyNumbersArray = onlyNumbers.split(" ");
+                        List<Double> doubleNumbers = new ArrayList<>();
+                        for (int i = 1; i < onlyNumbersArray.length; i++) {
+                            doubleNumbers.add(Double.parseDouble(String.valueOf(onlyNumbersArray[i])));
+                        }
+                        double x1 = doubleNumbers.get(0);
+                        double y1 = doubleNumbers.get(1);
+                        double x2 = doubleNumbers.get(2);
+                        double y2 = doubleNumbers.get(3);
 
                         return Math.abs(Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2)));
                     })
-                    .max(Double::compare);
-
+                    .max(Double::compare).orElseThrow();
+            System.out.println(maxLength);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.stream.Stream;
-
 public class Main {
 
     public static void main(String[] args) {
